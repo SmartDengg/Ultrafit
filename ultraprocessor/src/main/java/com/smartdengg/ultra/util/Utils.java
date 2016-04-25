170cf1140391637cf64d96eb6e9b2501361f5a19
SmartDengg <dengwei@lianjia.com>
2016-04-25 13:20:26 +0800

    提交 Utils类

---
+++ ultraprocessor/src/main/java/com/smartdengg/ultra/util/Utils.java
@@ -0,0 +1,19 @@
+package com.smartdengg.ultra.util;
+
+/**
+ * Created by Joker on 2016/4/25.
+ */
+public class Utils {
+
+    public static RuntimeException methodError(Class clazz, String message, Object... args) {
+        message = (args.length == 0) ? message : String.format(message, args);
+        return new IllegalArgumentException(clazz.getSimpleName() + ": " + message);
+    }
+
+    public static <T> T checkNotNull(T object, String message) {
+        if (object == null) {
+            throw new NullPointerException(message);
+        }
+        return object;
+    }
+}
