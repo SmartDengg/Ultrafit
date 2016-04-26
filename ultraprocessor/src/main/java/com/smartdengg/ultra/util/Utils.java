d66762f2939459fd9e498727a3e6b3a4859b9190
小鄧子 <Hi4Joker@gmail.com>
2016-04-26 23:47:54 +0800

    format code

--- ultraprocessor/src/main/java/com/smartdengg/ultra/util/Utils.java
+++ ultraprocessor/src/main/java/com/smartdengg/ultra/util/Utils.java
@@ -11,8 +11,9 @@ public class Utils {
     }
 
     public static <T> T checkNotNull(T object, String message) {
-        if (object == null)
-            throw new NullPointerException(message);
+
+        if (object == null) throw new NullPointerException(message);
+
         return object;
     }
 }
