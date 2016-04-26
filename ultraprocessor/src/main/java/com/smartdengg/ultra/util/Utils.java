71497b9cb46583bdb056cef9c219fd2f1db7461e
小鄧子 <Hi4Joker@gmail.com>
2016-04-26 23:46:01 +0800

    提交 utils类

--- ultraprocessor/src/main/java/com/smartdengg/ultra/util/Utils.java
+++ ultraprocessor/src/main/java/com/smartdengg/ultra/util/Utils.java
@@ -11,9 +11,8 @@ public class Utils {
     }
 
     public static <T> T checkNotNull(T object, String message) {
-        if (object == null) {
+        if (object == null)
             throw new NullPointerException(message);
-        }
         return object;
     }
 }
