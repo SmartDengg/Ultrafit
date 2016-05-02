f21b5653ec727cb8e6764743cdff4d5d25e4f23f
小鄧子 <Hi4Joker@gmail.com>
2016-05-02 16:31:04 +0800

    整理usecase 添加 single用法。慎用

--- Domain/src/main/java/com/smartdengg/domain/UseCase.java
+++ Domain/src/main/java/com/smartdengg/domain/UseCase.java
@@ -5,20 +5,25 @@ import android.support.annotation.NonNull;
 import com.orhanobut.logger.Logger;
 import com.smartdengg.common.Constants;
 import com.smartdengg.domain.rxcompat.SchedulersCompat;
+import com.smartdengg.domain.rxcompat.SmartExecutors;
 import com.smartdengg.ultra.RequestEntity;
 import com.smartdengg.ultra.UltraParserFactory;
 import java.util.Map;
 import rx.Observable;
 import rx.Observer;
+import rx.Single;
 import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func0;
 import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 import rx.subscriptions.Subscriptions;
 
 /**
  * Created by SmartDengg on 2016/2/22.
  */
+//@formatter:off
 public abstract class UseCase<R, S> {
 
     private Subscription subscription = Subscriptions.empty();
@@ -28,11 +33,10 @@ public abstract class UseCase<R, S> {
         this.subscribe(requestEntity, useCaseSubscriber, null);
     }
 
-    //@formatter:off
     @SuppressWarnings("unchecked")
-    public void subscribe(final R requestEntity, Observer<S> useCaseSubscriber, final Action1<RequestEntity> action1) {
+    public void subscribe(final R requestEntity, Observer<S> useCaseSubscriber, final Action1<RequestEntity> action) {
 
-        //*Be care of ConnectableObservable*//
+        /**be care of ConnectableObservable!!!*/
         this.subscription = Observable.defer(new Func0<Observable<RequestEntity>>() {
                                           @Override
                                           public Observable<RequestEntity> call() {
@@ -45,8 +49,8 @@ public abstract class UseCase<R, S> {
                                       .doOnNext(new Action1<RequestEntity>() {
                                           @Override
                                           public void call(RequestEntity requestEntity) {
-                                              if (action1 != null) {
-                                                  action1.call(requestEntity);
+                                              if (action != null) {
+                                                  action.call(requestEntity);
                                               } else {
                                                   Logger.t(Constants.OKHTTP_TAG, 0)
                                                         .d(requestEntity.toString());
@@ -77,48 +81,47 @@ public abstract class UseCase<R, S> {
 
     //@formatter:off
     @SuppressWarnings("unchecked")
-    public void subscribe(final R requestEntity, final Action1<? super S> onSuccess, final Action1<Throwable> onError, final Action1<RequestEntity> action1) {
+    public void subscribe(final R requestEntity, final Action1<? super S> onSuccess, final Action1<Throwable> onError, final Action1<RequestEntity> action) {
 
-        this.subscription = Observable.defer(new Func0<Observable<RequestEntity>>() {
-                                                @Override
-                                                  public Observable<RequestEntity> call() {
-
-                                                      return UltraParserFactory.createParser(requestEntity)
-                                                                               .parseRequestEntity()
-                                                                               .as(Observable.class);
-                                                  }
-                                              })
-                                              .doOnNext(new Action1<RequestEntity>() {
-                                                  @Override
-                                                  public void call(RequestEntity requestEntity) {
-                                                      if (action1 != null) {
-                                                          action1.call(requestEntity);
-                                                      } else {
-                                                          Logger.t(Constants.OKHTTP_TAG, 0)
-                                                                .d(requestEntity.toString());
-                                                      }
-                                                  }
-                                              })
-                                              .concatMap(new Func1<RequestEntity, Observable<S>>() {
-                                                  @Override
-                                                  public Observable<S> call(RequestEntity requestEntity) {
-                                                      return UseCase.this.interactor(requestEntity.getUrl(), requestEntity.getParamMap());
-                                                  }
-                                              })
-                                              .onBackpressureBuffer()
-                                              .takeFirst(new Func1<S, Boolean>() {
-                                                  @Override
-                                                  public Boolean call(S s) {
-                                                      return !subscription.isUnsubscribed();
-                                                  }
-                                              })
-                                              .compose(SchedulersCompat.<S>applyExecutorSchedulers())
-                                              .toSingle()
-                                              .subscribe(onSuccess, onError);
+        /**you can also use the operator {@link rx.Observable.toSingle}, it's simpler*/
+        this.subscription = Single.defer(new Func0<Single<RequestEntity>>() {
+                                      @Override
+                                      public Single<RequestEntity> call() {
+                                          return UltraParserFactory.createParser(requestEntity)
+                                                                   .parseRequestEntity()
+                                                                   .as(Single.class);
+                                      }
+                                  })
+                                  .map(new Func1<RequestEntity, RequestEntity>() {
+                                      @Override
+                                      public RequestEntity call(RequestEntity requestEntity) {
+                                          if (action != null) {
+                                              action.call(requestEntity);
+                                          } else {
+                                              Logger.t(Constants.OKHTTP_TAG, 0)
+                                                    .d(requestEntity.toString());
+                                          }
+                                          return null;
+                                      }
+                                  })
+                                  .flatMap(new Func1<RequestEntity, Single<S>>() {
+                                      @Override
+                                      public Single<S> call(RequestEntity requestEntity) {
+                                          return UseCase.this.interactorSingle(requestEntity.getUrl(), requestEntity.getParamMap());
+                                      }
+                                  })
+                                  .subscribeOn(Schedulers.from(SmartExecutors.eventExecutor))
+                                  .observeOn(AndroidSchedulers.mainThread())
+                                  .subscribe(onSuccess, onError);
     }
 
     public void unsubscribe() {
         if (!subscription.isUnsubscribed()) subscription.unsubscribe();
+    }
+
+    @CheckResult
+    protected Single<S> interactorSingle(@NonNull String url, @NonNull Map<String, String> params) {
+        throw new IllegalArgumentException("If you use the 'Single', you must implement this method");
     }
 
     @CheckResult
