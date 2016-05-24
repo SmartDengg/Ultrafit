d088c49902584693981dff568a94cd2101e29f79
小鄧子 <Hi4Joker@gmail.com>
2016-05-25 01:18:00 +0800

    :)

--- Model/src/main/java/com/smartdengg/model/request/CityListRequest.java
+++ Model/src/main/java/com/smartdengg/model/request/CityListRequest.java
@@ -1,7 +1,10 @@
 package com.smartdengg.model.request;
 
 import com.smartdengg.common.Constants;
+import com.smartdengg.ultra.annotation.Argument;
 import com.smartdengg.ultra.annotation.HttpGet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by SmartDengg on 2016/2/24.
@@ -12,4 +15,6 @@ public class CityListRequest extends BaseRequest {
    /* @Argument(parameter = "     ") private String test1 = "test1";
     @Argument private String test2 = "test2";
     @Argument(parameter = "test3") private String test3 = "test3";*/
+
+    @Argument(parameter = "test1") private List<String> test1 = new ArrayList<>();
 }
