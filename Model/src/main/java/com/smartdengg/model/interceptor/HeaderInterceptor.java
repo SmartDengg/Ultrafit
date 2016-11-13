ed703b32f9ea69dfea2855c74cfbaf1ad7b4b733
小鄧子 <Hi4Joker@gmail.com>
2016-11-13 22:29:54 +0800

    Fixed:调整依赖关系 continue-1

--- Model/src/main/java/com/smartdengg/model/interceptor/HeaderInterceptor.java
+++ Model/src/main/java/com/smartdengg/model/interceptor/HeaderInterceptor.java
@@ -11,21 +11,20 @@ import okhttp3.Response;
  */
 public class HeaderInterceptor implements Interceptor {
 
-    private HeaderInterceptor() {
-    }
+  private HeaderInterceptor() {
+  }
 
-    public static HeaderInterceptor createdInterceptor() {
-        return new HeaderInterceptor();
-    }
+  public static HeaderInterceptor createdInterceptor() {
+    return new HeaderInterceptor();
+  }
 
-    @Override
-    public Response intercept(Chain chain) throws IOException {
+  @Override public Response intercept(Chain chain) throws IOException {
 
-        Request.Builder builder = chain.request()
-                                       .newBuilder()
-                                       .addHeader("Accept-Encoding", "application/json")
-                                       .addHeader("Build-Time", BuildConfig.BUILD_TIME);
+    Request.Builder builder = chain.request()
+        .newBuilder()
+        .addHeader("Accept-Encoding", "application/json")
+        .addHeader("Build-Time", BuildConfig.BUILD_TIME);
 
-        return chain.proceed(builder.build());
-    }
+    return chain.proceed(builder.build());
+  }
 }
